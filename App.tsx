ecc528f62762a663b02ac1c7995d2bda0a4a3123
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-08-11 22:02:54 +0200

    feat: remove default text

--- App.tsx
+++ App.tsx
@@ -5,7 +5,6 @@ import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   )
