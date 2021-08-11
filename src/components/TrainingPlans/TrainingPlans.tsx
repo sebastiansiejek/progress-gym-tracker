cdb6e0807b47fdbec9a08f47b5e531c4cb357a51
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-08-11 23:13:02 +0200

    feat: exercices repeater

--- src/components/TrainingPlans/TrainingPlans.tsx
+++ src/components/TrainingPlans/TrainingPlans.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import { SafeAreaView, TextInput } from 'react-native'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
+import { Button, SafeAreaView, TextInput } from 'react-native'
+import { useImmer } from 'use-immer'
 
 export interface TrainingPlansProps {}
 
@@ -11,13 +12,58 @@ const TextInputStyled = styled(TextInput)`
 `
 
 const TrainingPlans: React.FunctionComponent<TrainingPlansProps> = ({}) => {
+  const [fields, setField] = useImmer<Array<{ name: string }>>([])
+
+  useEffect(() => {
+    console.log(fields)
+  }, [fields])
+
   return (
     <SafeAreaView
       style={{
         margin: 20,
       }}
     >
-      <TextInputStyled placeholder="Exercise" />
+      {fields.map((field, index) => (
+        <SafeAreaView
+          key={index}
+          style={{
+            marginBottom: 10,
+          }}
+        >
+          <TextInputStyled
+            placeholder="Exercise"
+            onChangeText={(value) => {
+              setField((draft) => {
+                draft[index] = {
+                  name: value,
+                }
+              })
+            }}
+          />
+          <Button
+            onPress={() => {
+              setField((draft) => {
+                draft.splice(index, 1)
+              })
+            }}
+            color="red"
+            title="Remove exercise"
+            accessibilityLabel="Click to remove exercise"
+          />
+        </SafeAreaView>
+      ))}
+      <Button
+        onPress={() => {
+          setField((draft) => {
+            draft.push({
+              name: '',
+            })
+          })
+        }}
+        title="Add new exercise"
+        accessibilityLabel="Click to add new exercise"
+      />
     </SafeAreaView>
   )
 }
